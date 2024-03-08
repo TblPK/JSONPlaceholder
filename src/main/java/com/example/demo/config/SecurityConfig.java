8c63a1c497be1f132f8c293a69247c2b4ab124c5
TblPK <sapaevmurat735@gmail.com>
2024-03-08 21:28:29 +0300

    Add basic security

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/posts/**").hasAnyRole("POSTS", "ADMIN")
+                        .requestMatchers("/api/users/**").hasAnyRole("USERS", "ADMIN")
+                        .requestMatchers("/api/albums/**").hasAnyRole("ALBUMS", "ADMIN")
+                        .requestMatchers("/login/**").anonymous()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("1")
+                .password("1")
+                .roles("POSTS")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
