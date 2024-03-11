825111b73eb20080133f051519e55cc72807df22
TblPK <sapaevmurat735@gmail.com>
2024-03-11 06:43:09 +0300

    delete unnecessary

--- src/main/java/com/example/demo/config/WebClientConfiguration.java
+++ src/main/java/com/example/demo/config/WebClientConfiguration.java
@@ -2,23 +2,11 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfiguration {
     private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
-
-    @Bean
-    public WebClient webClientWithTimeout() {
-        return WebClient
-                .builder()
-                .baseUrl(BASE_URL)
-                .defaultHeader(HttpHeaders.ACCEPT, MediaType.TEXT_HTML.toString())
-                .build();
-    }
 
     @Bean
     public RestClient restClient(RestClient.Builder builder) {
